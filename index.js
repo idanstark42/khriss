d01d7a26cd624535225b356ea951e4144fb87be7
Idan Stark <idanstark42@gmail.com>
2024-11-06 15:08:24 +0200

    added search tool

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const fs = require('fs')
+
+const search = require('./search')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/search', search)
+
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT)
+})
---
+++ search.js
@@ -0,0 +1,65 @@
+const { parse } = require('node-html-parser')
+
+module.exports = function search (req, res) {
+  const term = req.query.term
+  if (!term) {
+    res.status(400).send('Missing term')
+    return
+  }
+
+  Promise.all([searchArcanum(term), searchCoppermind(term)])
+    .then(([arcanum, coppermind]) => res.json({ arcanum, coppermind }))
+    .catch(error => {
+      console.error(error)
+      res.status(500).send('An error occurred')
+    })
+}
+
+const ARCANUM_URL = 'https://wob.coppermind.net/adv_search/'
+
+async function searchArcanum (searchTerm) {
+  const entriesInPage = pageHTML => {
+    const elements = pageHTML.querySelectorAll('.entry-content')
+    return elements.map(element => element.textContent)
+  }
+
+  const loadPageHTML = async page => {
+    const pageContent = await request(`${ARCANUM_URL}?ordering=rank&query=${searchTerm}&page=${page}`)
+    return parse(pageContent)
+  }
+
+  const entriesCount = pageHTML => {
+    const match = pageHTML.innerHTML.match(/Found (\d+) entries/)
+    return match ? parseInt(match[1]) : 0
+  }
+
+  const firstPageHTML = await loadPageHTML(1)
+  const numberOfEntries = entriesCount(firstPageHTML)
+  const numberOfPages = Math.ceil(numberOfEntries / 50)
+  console.log('Found %d WOB entries in %d pages', numberOfEntries, numberOfPages)
+  const pagesEtnries = await Promise.all([...Array(numberOfPages).keys()].map(async page => await entriesInPage(await loadPageHTML(page + 1))))
+  return pagesEtnries.flat()
+}
+
+
+// COPPERMIND
+
+const COPPERMIND_URL = 'https://coppermind.net/w/api.php'
+
+async function searchCoppermind (searchTerm) {
+  const pages = (await request(`${COPPERMIND_URL}?action=opensearch&search=${searchTerm}`, 'json'))[1]
+  console.log('Found %d wiki pages', pages.length)
+  const contents = []
+  for (const page of pages) {
+    const content = await request(`${COPPERMIND_URL}?action=raw&title=${page}`)
+    contents.push(content)
+  }
+  return contents
+}
+
+async function request (url, action = 'text') {
+  console.log(url)
+  const response = await fetch(url)
+  const data = await response[action]()
+  return data
+}
